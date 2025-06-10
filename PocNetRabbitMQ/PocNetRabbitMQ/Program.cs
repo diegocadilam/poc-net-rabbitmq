8c5b4d64a2202853e4bb250cd92e596b091d4740
diegocadilam <diego.rodrigues@cadilam.com.br>
2025-06-10 12:46:34 -0300

    Poc com Producer.

---
+++ PocNetRabbitMQ/PocNetRabbitMQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace PocNetRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var producer = new RabbitMQProducer();
+            var a = producer.Producer("Olá Diego").Result;
+
+        }       
+    }
+}
---
+++ PocNetRabbitMQ/PocNetRabbitMQ/RabbitMQProducer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using RabbitMQ.Client;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PocNetRabbitMQ
+{
+    public class RabbitMQProducer
+    {
+
+        public async Task<bool> Producer(string message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+            await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+            
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+            return true;    
+        }
+    }
+}
